5c4d3556ffef6cae7f8fd052c4cfa5d30d8e153d
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-05-20 16:36:33 -0300

    :sparkles: Criação do Recurso Tema e Relacionamento com a Classe Postagem

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -31,15 +32,26 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository postagemRepository;
 
+	@Autowired
+	private TemaRepository temaRepository;
+
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll() {
-		// SELECT * FROM tb_postagens;
+
+		/**
+		 * O Método executará a consulta: SELECT * FROM tb_postagens;
+		 */
 		return ResponseEntity.ok(postagemRepository.findAll());
 
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+
+		/**
+		 * O Método executará a consulta: SELECT * FROM tb_postagens WHERE id = ?; A
+		 * interrogação representa o valor inserido no parâmetro id do método getById
+		 */
 		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
@@ -47,40 +59,97 @@ public class PostagemController {
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> getAllByTitulo(@PathVariable String titulo) {
 
+		/**
+		 * O Método executará a consulta: SELECT * FROM tb_postagens WHERE titulo LIKE
+		 * '%?%"; A interrogação representa o valor inserido no parâmetro titulo do
+		 * método getAllByTitulo
+		 */
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 
 	}
 
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+
+		/** 
+		 * Verifica se o tema existe antes de persistir a postagem no Banco de dados
+		 * */
+		if (temaRepository.existsById(postagem.getTema().getId())) {
+
+			/**
+			 * O Método executará a consulta: INSERT INTO tb_postagens VALUES (titulo,
+			 * texto, data) VALUES (?, ?, ?); As interrogações representam os valores
+			 * inseridos nos respectivos atributos do objeto postagem, parâmetro do método
+			 * post.
+			 */
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		}
+		
+		/** 
+		 * Caso o tema não exista, retorna um Bad Request informando que o tema não existe
+		 * */
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O Tema não existe!", null);
 	}
 
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
 
-		if(postagem.getId() == null)
+		/**
+		 * Verifica se o id é nulo. Se for nulo, retorna o HTTP Status 400 - BAD_REQUEST
+		 */
+		if (postagem.getId() == null)
 			return ResponseEntity.badRequest().build();
-		
-		if (postagemRepository.existsById(postagem.getId())) 
+
+		/**
+		 * Antes de atualizar, verifica se a postagem existe. Se existir, atualiza
+		 */
+		if (postagemRepository.existsById(postagem.getId())) {
 			
-			return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+			/** 
+			 * Verifica se o tema existe antes de atualizar a postagem no Banco de dados
+			 * */
+			if (temaRepository.existsById(postagem.getTema().getId()))
+				/**
+				 * O Método executará a consulta: UPDATE tb_postagens SET titulo = ?, texto = ?,
+				 * data = ? WHERE id = ?; As interrogações representam os valores inseridos nos
+				 * respectivos atributos do objeto postagem, parâmetro do método post.
+				 */
+				return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
 		
+			/** 
+			 * Caso o tema não exista, retorna um Bad Request informando que o tema não existe
+			 * */
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O Tema não existe!", null);
+			
+		}
+		/**
+		 * Se a postagem não existir, retorna o HTTP Status 404 - NOT_FOUND
+		 */
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		
+
 	}
 
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
-		
+
+		/**
+		 * Busca a postagem pelo id e guarda o resultado no Optional postagem
+		 */
 		Optional<Postagem> postagem = postagemRepository.findById(id);
-		
-		if(postagem.isEmpty())
+
+		/**
+		 * Verifica se o Optional postagem está vazio. Se estiver vazio, retorna o HTTP
+		 * Status 404 - NOT_FOUND
+		 */
+		if (postagem.isEmpty())
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-		
+
+		/**
+		 * Caso contrário, o Método executará a consulta: DELETE FROM tb_postagens WHERE
+		 * id = ?; A interrogação representa parâmetro id do método delete.
+		 */
 		postagemRepository.deleteById(id);
-		
-		// DELETE FROM tb_postagens WHERE id=?;
+
 	}
 }
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -21,8 +24,7 @@ public class Postagem {
 	@Id // Primary Key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
 	private Long id;
-	
-	
+		
 	@Column(length = 100)
 	@NotBlank(message = "O atributo título é obrigatório!")
 	@Size(min = 5, max = 100, message = "O atributo título deve ter no minimo 5 e no máximo 100 caracteres.")
@@ -32,12 +34,16 @@ public class Postagem {
 	@Column(length = 1000)
 	@NotBlank(message = "O atributo texto é obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve ter no minimo 10 e no máximo 1000 caracteres.")
-	@Pattern(regexp = "^[^0-9].*", message = "O título não pode ser apenas numérico")
+	@Pattern(regexp = "^[^0-9].*", message = "O texto não pode ser apenas numérico")
 	private String texto;
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
 
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public Long getId() {
 		return id;
 	}
@@ -68,6 +74,14 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 }
